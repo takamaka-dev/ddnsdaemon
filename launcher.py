154d9a6c4c00de18bdfc74e66489164686a2627b
giovanni.antino@h2tcoin.com <giovanni.antino@h2tcoin.com>
2021-09-02 17:59:47 +0200

    launcher stub

--- launcher.py
+++ launcher.py
@@ -2,6 +2,7 @@ import os
 import platform
 from pathlib import Path
 
+pid_filename = "ddnsdaemon.pid"
 linux_tempdir = "/tmp"
 
 if __name__ == "__main__":
@@ -11,7 +12,12 @@ if __name__ == "__main__":
     print("I'm launcher running on: " + curr_platform)
     print("APPDATA: " + str(win_appdata))
     if curr_platform == "Windows":
-        pid_file_path = os.path.dirname(str(win_appdata)[:-len("Roaming")])
-        parent = Path(win_appdata).resolve().parent
+        # pid_file_path = os.path.dirname(str(win_appdata)[:-len("Roaming")])
+        app_data = Path(win_appdata).resolve().parent
         print("APPDATA DIR: " + str(pid_file_path))
-        print("parent: " + str(parent))
+        print("parent: " + str(app_data))
+        pid_file_path = os.path.join(app_data, "Temp", pid_filename)
+    elif curr_platform == "Linux":
+        pid_file_path = os.path.join(linux_tempdir, pid_filename)
+
+    print("Pid File is " + str(pid_file_path))
