ff6493c979956b77f3318354baf853238294a1eb
giovanni.antino@h2tcoin.com <giovanni.antino@h2tcoin.com>
2021-09-02 17:48:08 +0200

    launcher stub

--- launcher.py
+++ launcher.py
@@ -3,8 +3,12 @@ import platform
 
 linux_tempdir = "/tmp"
 
-
 if __name__ == "__main__":
     curr_platform = platform.system()
+    win_appdata = os.getenv('APPDATA')
+    pid_file_path = None
     print("I'm launcher running on: " + curr_platform)
-    print("APPDATA: " + str(os.getenv('APPDATA')))
+    print("APPDATA: " + str(win_appdata))
+    if curr_platform == "Windows":
+        pid_file_path = os.path.join(win_appdata, "..")
+        print("APPDATA DIR: " + str(pid_file_path))
